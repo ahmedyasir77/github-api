3d3b8c06af02be2e81fe208ea87fbe88f9a828f7
Yasir Ahmed <ahmedyasir77@gmail.com>
2021-07-17 22:10:28 -0500

    Finished submissions function

--- hn_submissions.py
+++ hn_submissions.py
@@ -1,7 +1,7 @@
 from operator import itemgetter
 
 import requests
-from requests.models import Response
+#from requests.models import Response
 
 # Make and API call and store the response. 
 url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
@@ -11,6 +11,7 @@ print(f"Status code: {r.status_code}")
 # Process information about each submission.
 submission_ids = r.json()
 submission_dicts = []
+
 for submission_id in submission_ids[:30]:
     # Make a separate API call for each submisson.
     url = (f"https://hacker-news.firebaseio.com/v0.item/{submission_id}.json")
@@ -25,4 +26,11 @@ for submission_id in submission_ids[:30]:
         'comments': response_dict['descendants',]
     }
     submission_dicts.append(submission_dict)
+    
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
+
+for submission_dict in submission_dicts:
+    print(f"\nTitle: {submission_dict['title']}")
+    print(f"Discussion link: {submission_dict['hn_link']}")
+    print(f"Comments: {submission_dict['comments']}")
     
