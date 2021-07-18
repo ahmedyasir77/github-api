975ab9571f489ca8a99e043d6f8b86e496853e7a
Yasir Ahmed <ahmedyasir77@gmail.com>
2021-07-18 12:21:19 -0500

    Added type in dictionary

--- hn_submissions.py
+++ hn_submissions.py
@@ -23,7 +23,8 @@ for submission_id in submission_ids[:30]:
     submission_dict = {
         'title': response_dict['title'],
         'hn_link': (f"http://news.ycombinator.com/item?id={submission_id}"),
-        'comments': response_dict['descendants',]
+        'comments': response_dict['descendants'],
+        'type': response_dict['type']
     }
     submission_dicts.append(submission_dict)
     
@@ -33,4 +34,5 @@ for submission_dict in submission_dicts:
     print(f"\nTitle: {submission_dict['title']}")
     print(f"Discussion link: {submission_dict['hn_link']}")
     print(f"Comments: {submission_dict['comments']}")
+    print(f"Type: {submission_dict['type']}")
     
